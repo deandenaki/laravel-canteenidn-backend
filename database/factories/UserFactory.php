887d9154689507d3245aa25ca95b10dcd9acb8ac
Dean Denaki <sonicmailfaster@gmail.com>
2024-10-18 11:04:06 +0700

    feat: login authentication

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => 'Dean Denaki',
+            'email' => 'deandenaki@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
+            'phone' => '0392301900329'
+        ];
+    }
+
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,42 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link"
+                            href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
+                    </li>
+                    <li class="{{ Request::is('dashboard-ecommerce-dashboard') ? 'active' : '' }}">
+                        <a class="nav-link"
+                            href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
+                    </li>
+                </ul>
+            </li>
+            {{-- <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Google
+                        Maps</span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="gmaps-advanced-route.html">Advanced Route</a></li>
+                    <li><a href="gmaps-draggable-marker.html">Draggable Marker</a></li>
+                    <li><a href="gmaps-geocoding.html">Geocoding</a></li>
+                    <li><a href="gmaps-geolocation.html">Geolocation</a></li>
+                    <li><a href="gmaps-marker.html">Marker</a></li>
+                    <li><a href="gmaps-multiple-marker.html">Multiple Marker</a></li>
+                    <li><a href="gmaps-route.html">Route</a></li>
+                    <li><a href="gmaps-simple.html">Simple</a></li>
+                </ul>
+            </li> --}}
+        </ul>
+    </aside>
+</div>
